0816505551d0a5d3a5b33d437f9b687880b69790
Mehmet Maytahan <maytahan71@gmail.com>
2022-03-20 15:08:36 +0300

    desing changed

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const btn = document.querySelector("#btn");
 const span = document.querySelector(".span");
+const spanRgb = document.querySelector(".rgb");
 
 const rgb = () => {
   return Math.floor(Math.random() * 255);
 };
 
 const handleChangeColor = () => {
-  span.style.color = `rgb(${rgb()},${rgb()},${rgb()})`;
-  span.style.background = `rgb(${rgb()},${rgb()},${rgb()})`;
-
-  btn.style.color = `rgb(${rgb()},${rgb()},${rgb()})`;
-  btn.style.background = `rgb(${rgb()},${rgb()},${rgb()})`;
+  document.body.style.background = `rgb(${rgb()},${rgb()},${rgb()})`;
+  span.style.color = `rgb(255,255,255)`;
+  spanRgb.textContent = `rgb(${rgb()},${rgb()},${rgb()})`;
 };
 
 btn.addEventListener("click", handleChangeColor);
